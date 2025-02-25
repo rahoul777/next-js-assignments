8563146c986ad6b6911e5da099b2534e8cda7bcd
Rahul Shambharkar <rahoul777shambharkar@gmail.com>
2025-02-25 07:41:03 +0530

    install framer-motion

--- securecity-dashboard/src/pages/_app.js
+++ securecity-dashboard/src/pages/_app.js
@@ -1,10 +1,24 @@
 import Layout from '@/components/Layout';
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+import { useRouter } from 'next/router';
+import { motion, AnimatePresence } from 'framer-motion';
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
